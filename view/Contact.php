43b2d0e33edcec44a2db05228347fa5c35234e96
nolan32 <jordy.duces@hotmail.frgit config --global user.email jordy.duces@hotmail.frgit config --global user.email jordy.duces@hotmail.fr>
2017-07-04 16:04:46 +0200

    ajout de l'affichage par groupe + affichage des groupes dans le tableau Contact general

--- view/Contact.php
+++ view/Contact.php
@@ -22,18 +22,20 @@ try {
         <th>Entreprise</th>
         <th>Adresse</th>
         <th>Numero de téléphone</th>
+        <th>Groupe</th
         <th>Supprimer</th>
         <th>Modifier</th>
       </tr><?php
-$response = $bdd->query('SELECT * FROM Contact');
+$response = $bdd->query('SELECT * FROM Contact JOIN appartenir ON Contact.id = appartenir.fk_contact JOIN groupe ON appartenir.fk_groupe = groupe.id');
 while ($donnees = $response->fetch()) {
-$id = $donnees['id'];
+$id = $donnees['fk_contact'];
 $nom = $donnees['Nom'];
 $prenom = $donnees['Prenom'];
 $birthday = $donnees['birthday'];
 $Entreprise = $donnees['Entreprise'];
 $Adresse = $donnees['Adresse'];
 $NumTel = $donnees['NumTel'];
+$group1 = $donnees['fk_groupe'];
 
 ?>
       <tr>
@@ -60,7 +62,10 @@ $NumTel = $donnees['NumTel'];
 
         ?></td>
         <td><?php
-          echo $NumTel,'
+          echo $NumTel;
+          ?></td>
+          <td><?php
+          echo $group1,'
           <td><button type="button">Supprimer</button></td>
           <td><button type="button">Modifier</button></td>';
         }
@@ -68,7 +73,132 @@ $NumTel = $donnees['NumTel'];
         </td>
       </tr>
     </table>
+    <h4>Famille :</h4>
+    <table>
+      <tr>
+        <th>id</th>
+        <th>Nom</th>
+        <th>Prénom</th>
+        <th>Date de naissance</th>
+        <th>Entreprise</th>
+        <th>Adresse</th>
+        <th>Numero de téléphone</th>
+        <th>Groupe</th>
+        <th>Supprimer</th>
+        <th>Modifier</th>
+      </tr>
+      <tr>
+        <?php
+        $response = $bdd->query('SELECT * FROM Contact JOIN appartenir ON Contact.id = appartenir.fk_contact JOIN groupe ON appartenir.fk_groupe = groupe.id WHERE groupe.id = 1');
+        while ($donnees = $response->fetch()) {
+        $id = $donnees['fk_contact'];
+        $nom = $donnees['Nom'];
+        $prenom = $donnees['Prenom'];
+        $birthday = $donnees['birthday'];
+        $Entreprise = $donnees['Entreprise'];
+        $Adresse = $donnees['Adresse'];
+        $NumTel = $donnees['NumTel'];
+        $group1 = $donnees['fk_groupe'];
+         ?>
+      </tr>
+      <tr>
+        <td><?php
+          echo $id;
 
+        ?></td>
+        <td><?php
+          echo $nom;
+
+        ?>
+      </td>
+      <td><?php
+         echo $prenom;
+        ?></td>
+       <td><?php
+        echo $birthday;
+        ?></td>
+        <td><?php
+          echo $Entreprise;
+        ?></td>
+        <td><?php
+          echo $Adresse;
+
+        ?></td>
+        <td><?php
+          echo $NumTel;
+          ?></td>
+          <td><?php
+          echo $group1,'
+          <td><button type="button">Supprimer</button></td>
+          <td><button type="button">Modifier</button></td>';
+        }
+        ?>
+        </td>
+      </tr>
+    </table>
+    <h4>Boulot :</h4>
+    <table>
+      <tr>
+        <th>id</th>
+        <th>Nom</th>
+        <th>Prénom</th>
+        <th>Date de naissance</th>
+        <th>Entreprise</th>
+        <th>Adresse</th>
+        <th>Numero de téléphone</th>
+        <th>Groupe</th>
+        <th>Supprimer</th>
+        <th>Modifier</th>
+      </tr>
+      <tr>
+        <?php
+        $response = $bdd->query('SELECT * FROM Contact JOIN appartenir ON Contact.id = appartenir.fk_contact JOIN groupe ON appartenir.fk_groupe = groupe.id WHERE groupe.id = 2');
+        while ($donnees = $response->fetch()) {
+        $id = $donnees['fk_contact'];
+        $nom = $donnees['Nom'];
+        $prenom = $donnees['Prenom'];
+        $birthday = $donnees['birthday'];
+        $Entreprise = $donnees['Entreprise'];
+        $Adresse = $donnees['Adresse'];
+        $NumTel = $donnees['NumTel'];
+        $group1 = $donnees['fk_groupe'];
+         ?>
+      </tr>
+      <tr>
+        <td><?php
+          echo $id;
+
+        ?></td>
+        <td><?php
+          echo $nom;
+
+        ?>
+      </td>
+      <td><?php
+         echo $prenom;
+        ?></td>
+       <td><?php
+        echo $birthday;
+        ?></td>
+        <td><?php
+          echo $Entreprise;
+        ?></td>
+        <td><?php
+          echo $Adresse;
+
+        ?></td>
+        <td><?php
+          echo $NumTel;
+          ?></td>
+          <td><?php
+          echo $group1,'
+          <td><button type="button">Supprimer</button></td>
+          <td><button type="button">Modifier</button></td>';
+        }
+        ?>
+        </td>
+      </tr>
+    </table>
 
 
 
